56eeeaaa944707d4bccfc73ef153af8502e854af
Isaac Obuya <Szack4477@gmail.com>
2020-04-03 10:16:26 +0300

    write tests for input success=true/false

--- src/Input.js
+++ src/Input.js
@@ -1,16 +1,38 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Input extends Component {
-    render() {
-        return (
-            <div></div>
-        )
-    }
+  render() {
+    return (
+      <div data-test="component-input">
+        {this.props.success === false ? (
+          <>
+            <form className="guessInput">
+              <input
+                type="text"
+                id="guessWord"
+                data-test="input-box"
+                placeholder="Enter your gues here"
+              />
+            </form>
+            <button
+              type="submit"
+              data-test="submit-button"
+              className="submit-btn"
+            >
+              Submit
+            </button>
+          </>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {};
-}
+const mapStateToProps = ({ success }) => {
+  return { success };
+};
 
 export default connect(mapStateToProps)(Input);
--- src/Input.test.js
+++ src/Input.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow, ShallowWrapper } from "enzyme";
 
 import Input from "./Input";
-import {findByTestAttr, storeFactory} from "../test/testUtils";
+import { findByTestAttr, storeFactory } from "../test/testUtils";
 
 //write set up function to connect to redux store
 
@@ -11,46 +11,62 @@ import {findByTestAttr, storeFactory} from "../test/testUtils";
  * @param {object} initialState - Initial state for this setup
  * @returns {ShallowWrapper}
  */
-const setup = (initialState={}) => {
-    // create a redux store with the store factory utility
-    const store = storeFactory(initialState);
-    // we need to pass a store as a prop to the connected component
-    const wrapper = shallow(<Input store={store}/>).dive();
-    
-}
+const setup = (initialState = {}) => {
+  // create a redux store with the store factory utility
+  const store = storeFactory(initialState);
+  // we need to pass a store as a prop to the connected component
+  const wrapper = shallow(<Input store={store} />).dive();
+  const wrapper2 = wrapper.dive();
+  return wrapper2;
+};
 
 describe("render Input Component", () => {
+  let wrapper;
 
-    describe("word has not been guessed", () => {
-        it("renders component without error", () => {
+  beforeEach(() => {
+    const initialState = { success: false };
+    wrapper = setup(initialState);
+  });
+  describe("word has not been guessed", () => {
+    it("renders component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
 
-        });
+    it("renders the input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
+    });
 
-        it("renders the input box", () => {
+    it("renders the submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
+    });
+  });
 
-        });
+  describe("word has been guessed", () => {
+    let wrapper;
 
-        it("renders the submit button", () => {
+    beforeEach(() => {
+      const intialState = { success: true };
+      wrapper = setup(intialState);
+    });
 
-        });
-    })
+    it("renders component without error", () => {
+        const component = findByTestAttr(wrapper, "component-input");
+        expect(component.length).toBe(1);
+    });
 
-    describe("word has been guessed", () => {
-     
-        it("renders component without error", () => {
+    it("does not renders the input box", () => {
+        const inputBox = findByTestAttr(wrapper, "input-box");
+        expect(inputBox.length).toBe(0);
+    });
 
-        });
-
-        it("does not renders the input box", () => {
-
-        });
-
-        it("does not renders the submit button", () => {
-
-        });
-    })
+    it("does not renders the submit button", () => {
+        const submitButton = findByTestAttr(wrapper, "submit-button");
+        expect(submitButton.length).toBe(0);
+    });
+  });
 });
 
-describe("update success state", () => {
-
-})
+describe("update success state", () => {});
